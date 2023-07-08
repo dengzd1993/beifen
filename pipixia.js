9ad745d77868d3ba512893ba5de64e3dc45450c1
dengzd1993 <136882819+dengzd1993@users.noreply.github.com>
2023-07-08 17:26:42 +0800

    Update pipixia.js

--- pipixia.js
+++ pipixia.js
@@ -26,7 +26,7 @@ QuantumultX:
 hostname = *.snssdk.com
 
 **************************
-Surge4 或 Loon:
+Surge4 or Loon:
 
 [Script]
 http-response ^https?://.*\.snssdk\.com/bds/(feed/stream|comment/cell_reply|cell/cell_comment|cell/detail|ward/list|user/favorite|user/cell_coment|user/cell_userfeed|user/publish_list) requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Super.js
@@ -36,7 +36,7 @@ hostname = *.snssdk.com
 
 **************************/
 
-var body = $response.body.replace(/id\":([0-9]{15,})/g， 'id":"$1str"');
+var body = $response.body.replace(/id\":([0-9]{15,})/g, 'id":"$1str"');
 body = JSON.parse(body);
 if (body.data.data) {
     obj = body.data.data;
@@ -50,7 +50,7 @@ if (body.data.data) {
 
 if (obj instanceof Array) {
     if (obj != null) {
-        for (var i 在 obj) {
+        for (var i in obj) {
             if (obj[i].ad_info != null) {
                 obj.splice(i, 1);
             }
@@ -58,7 +58,7 @@ if (obj instanceof Array) {
                 if (obj[i].item.video != null) {
                     obj[i].item.video.video_download.url_list = obj[i].item.origin_video_download.url_list;
                 }
-                for (var j 在 obj[i].item.comments) {
+                for (var j in obj[i].item.comments) {
                     if (obj[i].item.comments[j].video != null) {
                         obj[i].item.comments[j].video_download.url_list = obj[i].item.comments[j].video.url_list;
                     }
@@ -76,7 +76,7 @@ if (obj instanceof Array) {
         if (obj.item.video != null) {
             obj.item.video.video_download.url_list = obj.item.origin_video_download.url_list;
         }
-        for (var j 在 obj.item.comments) {
+        for (var j in obj.item.comments) {
             if (obj.item.comments[j].video != null) {
                 obj.item.comments[j].video_download.url_list = obj.item.comments[j].video.url_list;
             }
@@ -89,10 +89,10 @@ if (obj instanceof Array) {
     }
 }
 body = JSON.stringify(body);
-body = body.replace(/id\":\"([0-9]{15,})str\"/g， 'id":$1');
-body = body.replace(/\"can_download\":false/g， '"can_download":true');
-body = body.replace(/tplv-ppx-logo.image/g， '0x0.gif');
-body = body.replace(/tplv-ppx-logo/g， '0x0');
+body = body.replace(/id\":\"([0-9]{15,})str\"/g, 'id":$1');
+body = body.replace(/\"can_download\":false/g, '"can_download":true');
+body = body.replace(/tplv-ppx-logo.image/g, '0x0.gif');
+body = body.replace(/tplv-ppx-logo/g, '0x0');
 $done({
     body
 });
